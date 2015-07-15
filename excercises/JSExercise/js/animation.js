ff98f06495b9cafe8ef18b7d91ca72f03e35e9da
RagT <raghutir@gmail.com>
2015-07-14 23:28:31 -0700

    Addition of Exercise involving before and after functions

---
+++ excercises/JSExercise/js/animation.js
@@ -0,0 +1,17 @@
+var animationHelper = AniJS.getHelper();
+
+animationHelper.checkInput = function(e, animationContext){
+	var username = $('#username').val();
+	var password = $('#pwd').val();
+	if(username.length > 3 && password.length > 6){
+		$('form').css('background-color', 'green');
+		animationContext.run();
+	} else {
+		$('#form-heading').html('Invalid Password/Username combination.');
+	}
+}
+
+animationHelper.reset = function(e, animationContext){
+	$('h1').text('You have succesfully logged in.');
+	$('header').css('background-color', 'green');
+}
